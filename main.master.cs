408025df8a70c8a2bcd2abe87dcc6f89e656ab3a
woo001016 <97575061+woo001016@users.noreply.github.com>
2023-03-13 20:36:53 +0900

    Add files via upload

---
+++ main.master.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Configuration;
+
+public partial class MainMaster : System.Web.UI.MasterPage {
+
+	protected void Page_Load(object sender, EventArgs e) {
+		if (!IsPostBack) {
+
+
+			this.OnBeforePostBack();
+		}
+
+	}
+
+	protected virtual void OnBeforePostBack() {
+
+		
+	}
+
+	public virtual string Title
+	{
+		get
+		{
+			return Master.Title;
+		}
+		set
+		{
+			Master.Title = value;
+		}
+	}
+
+	public virtual void HideHeader() {
+		Master.HideHeader();
+	}
+
+	public virtual void HideFooter() {
+		Master.HideFooter();
+	}
+
+}
