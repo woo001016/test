f5fdde4927de8c5a0b8347f3ab023c9727af9e2a
woo001016 <97575061+woo001016@users.noreply.github.com>
2023-03-13 20:42:02 +0900

    Add files via upload

--- business/default.aspx.cs
+++ business/default.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class business_default : System.Web.UI.Page {
+public partial class business_ev_default : System.Web.UI.Page {
 	protected void Page_Load(object sender, EventArgs e) {
 
 		Response.Redirect(Lang.Href("/business/ev-relay/info"));
